45d0936ecfca4f29907e73d7062f1c8adeed3412
magriker <magken7@gmail.com>
2025-07-06 17:02:34 +0900

    commit

---
+++ src/components/CityList.tsx
@@ -0,0 +1,5 @@
+import styles from "./CityList.module.css";
+
+export function CityList() {
+  return <ul className={styles.cityList}></ul>;
+}
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,13 +1,14 @@
 import styles from "./Sidebar.module.css";
 import Logo from "./Logo";
 import { AppNav } from "./AppNav";
+import { Outlet } from "react-router-dom";
 
 export function Sidebar() {
   return (
     <div className={styles.sidebar}>
       <Logo></Logo>
       <AppNav></AppNav>
-      <p>List of cityes</p>
+      <Outlet></Outlet>
       <footer className={styles.footer}>
         <p className={styles.copyright}>
           &copy; Copyright {new Date().getFullYear()} by WorldWise inc
